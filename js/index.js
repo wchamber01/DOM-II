ba370d6b49754cd483ba0d26ea7d2e9d0c0778b0
wchamber01 <52251104+wchamber01@users.noreply.github.com>
2019-10-29 19:15:39 -0400

    added some events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,65 @@
 // Your code goes here
+
+//**********BEGIN MOUSEENTER EVENT**********//
+const busImg = document.querySelector('#bus');
+busImg.addEventListener('mouseenter', () => {
+    busImg.style.transform = 'scale(2)';
+    busImg.style.transition = 'all 0.3s';
+})
+//**********END MOUSEENTER EVENT**********//
+
+
+
+//**********BEGIN MOUSELEAVE EVENT**********//
+busImg.addEventListener('mouseleave', () => {
+    busImg.style.transform = 'scale(1)';
+    busImg.style.transition = 'all 0.3s';
+})
+//**********END MOUSELEAVE EVENT**********//
+
+
+
+//**********BEGIN SCROLL EVENT**********//
+const adventureImg = document.querySelector('#adventure');
+window.addEventListener('scroll', () => {
+    adventureImg.src = 'https://media.giphy.com/media/ZedVN1A1uQX25J8Oou/giphy.gif';
+    adventureImg.style.transition = 'all 1s';
+})
+//**********END SCROLL EVENT**********//
+
+
+
+//**********BEGIN CURSOR EVENT**********//
+// const cursor = window.style.cursor;
+// window.addEventListener('mousemove', () => {
+// window.style.cursor = 'img/plane.svg';
+// })
+//**********END CURSOR EVENT**********//
+
+
+
+//**********BEGIN RESIZE EVENT**********//
+const dark = document.body.style.backgroundColor;
+window.addEventListener('resize', () => {
+    document.body.style.backgroundColor = rgba(0,0,0,0.5)
+})
+//**********END RESIZE EVENT**********//
+
+
+
+//**********BEGIN CLICK EVENT**********//
+const sun = document.querySelector('#sun');
+sun.addEventListener('click', () => {
+    document.querySelector('#destination').src = 'https://images.unsplash.com/7/sunset-hair.jpg?ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80'
+})
+
+const mountain = document.querySelector('#mountain');
+mountain.addEventListener('click', () => {
+    document.querySelector('#destination').src = 'https://images.unsplash.com/photo-1476611338391-6f395a0ebc7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80'
+})
+
+const island = document.querySelector('#island');
+island.addEventListener('click', () => {
+    document.querySelector('#destination').src = 'https://images.unsplash.com/photo-1529815481058-55e5b656f6d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+})
+//**********END CLICK EVENT**********//
